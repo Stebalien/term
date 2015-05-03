e83e1e12e1e67d6689250f6d1a904c07a4edc175
Alex Crichton <alex@alexcrichton.com>
2015-05-03 00:03:13 -0700

    Typos

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,8 @@
 //!
 //! # Usage
 //!
-//! This crates is [on crates.io](https://crates.io/crates/term) and can be
-//! used by adding `rand` to your dependencies in your project's `Cargo.toml`.
+//! This crate is [on crates.io](https://crates.io/crates/term) and can be
+//! used by adding `rand` to the dependencies in your project's `Cargo.toml`.
 //!
 //! ```toml
 //! [dependencies]
